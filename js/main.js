c9b8fc1f1596f6a0f689281a59b0105453b3dfbf
MarcTempest <dragonatore45@gmail.com>
2024-12-24 15:07:09 +0000

    Merry Christmas

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+function next(){
+    window.location.href='./reveal.html'
+}
+function next1(){
+    window.location.href='./love.html'
+}
+function next2(){
+    window.location.href='./mery.html'
+}
